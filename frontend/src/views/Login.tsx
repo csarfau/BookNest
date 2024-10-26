febaaaa72c5e05049779488466a2ae8f85f84c2f
César <csarfau@gmail.com>
2024-10-26 17:29:15 -0300

    feature: Basic of the Login page

--- frontend/src/views/Login.tsx
+++ frontend/src/views/Login.tsx
@@ -1,7 +1,23 @@
+import { Link } from "react-router-dom";
+
 export default function Login() {
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      Login
+    <div className="login-signup-form animated fadeInDown">
+      <div className="form">
+        <form onSubmit={onSubmit}>
+          <h1 className="title">Acessar sua conta</h1>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Senha" />
+          <button className="btn btn-block">Entrar</button>
+          <p className="message">
+            Não possui uma conta? <Link to="/signup">Criar conta</Link>
+          </p>
+        </form>
+      </div>
     </div>
   );
 }
