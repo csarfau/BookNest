1a8bb068ef25268a33cc903c7ce7aaa7b7226795
César <csarfau@gmail.com>
2024-10-26 17:06:41 -0300

    feature: Basic of the defaultLayout

--- frontend/src/components/DefaultLayout.tsx
+++ frontend/src/components/DefaultLayout.tsx
@@ -1,7 +1,37 @@
+import { Link, Navigate, Outlet } from "react-router-dom";
+import { useUser } from "../contexts/UserContext";
+
 export default function DefaultLayout() {
+  const { user, token } = useUser();
+
+  if(!token) {
+    return <Navigate to="/login" />
+  }
+
+  const onLogout = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+  }
+
   return (
-    <div>
-      DefaultLayout
+    <div id="defaultLayout">
+      <aside>
+        <Link to="/dashboard">Painel</Link>
+        <Link to="/books">Livros</Link>
+      </aside>
+      <div className="content">
+        <header>
+          <div>
+            Header
+          </div>
+          <div>
+            {user?.name}
+            <a href="#" onClick={onLogout} className="btn-logout">Sair</a>
+          </div>
+        </header>
+        <main>
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 }
