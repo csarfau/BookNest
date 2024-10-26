7747a7fd3511bb622c652a4ce18bf72e707d98e2
César <csarfau@gmail.com>
2024-10-26 16:31:20 -0300

    feature: add contextProvider to manage user state

---
+++ frontend/src/contexts/UserContext.tsx
@@ -0,0 +1,53 @@
+import { createContext, ReactNode, useContext, useState } from "react";
+import { IUser } from "../../types/user";
+
+// Tipagem para o children do context
+type UserContextProps = {
+  children: ReactNode;
+};
+
+// Tipagem do que irá conter no contexto
+type UserContextType = {
+  user: IUser | null;
+  token: string | null;
+  setUser: (user: IUser | null) => void;
+  setToken: (token: string) => void;
+};
+
+// Criando o contexto de usuário para autenticação
+const UserContext = createContext<UserContextType>({
+  user: null,
+  token: null,
+  setUser: () => {},
+  setToken: () => {}
+});
+
+// Criando um contextProvider, que vai envolver toda a aplicação
+export const ContextProvider = ({ children }: UserContextProps) => {
+  const [user, setUser] = useState<IUser | null>(null);
+  const [token, _setToken] = useState(localStorage.getItem("ACCESS_TOKEN"));
+
+  const setToken = (token: string) => {
+    _setToken(token);
+    if (token) {
+      localStorage.setItem("ACCESS_TOKEN", token);
+    } else {
+      localStorage.removeItem("ACCESS_TOKEN");
+    }
+  };
+
+  return (
+    <UserContext.Provider value={{
+        user,
+        token,
+        setUser,
+        setToken,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+// Exportando o contexto para usar nos componentes
+export const useUser = () => useContext(UserContext); 
--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import Login from "./views/Login";
 import SignUp from "./views/SignUp";
 import Books from "./views/Books";
@@ -10,6 +10,24 @@ import Dashboard from "./views/Dashboard";
 // Criação das rotas com o React Router Dom
 const router = createBrowserRouter([
   {
+    path: "/", // Grupo de rotas para usuários autenticados
+    element: <DefaultLayout />,
+    children: [
+      {
+        path: "/", // Rota para redirecionar 
+        element: <Navigate to="/books" />
+      },
+      {
+        path: "/dashboard", // Rota de dashboard dos livros
+        element: <Dashboard />,
+      },
+      {
+        path: "/books", // Rota com a lista de livros do usuário
+        element: <Books />,
+      },
+    ],
+  },
+  {
     path: "/", // Grupo de rotas para usuários desconhecidos
     element: <GuestLayout />,
     children: [
@@ -20,20 +38,6 @@ const router = createBrowserRouter([
       {
         path: "/signup", // Rota de registro
         element: <SignUp />,
-      },
-    ],
-  },
-  {
-    path: "/", // Grupo de rotas para usuários autenticados
-    element: <DefaultLayout />,
-    children: [
-      {
-        path: "/dashboard", // Rota de dashboard dos livros
-        element: <Dashboard />,
-      },
-      {
-        path: "/books", // Rota com a lista de livros do usuário
-        element: <Books />,
       },
     ],
   },
