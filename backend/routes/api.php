824ffb34dfdc5c35d64066d0fa39bfe7ab5a6154
César <csarfau@gmail.com>
2024-10-27 14:43:31 -0300

    feature: Implement request validation for store and update books

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,8 +20,9 @@ Route::middleware('auth:sanctum')->group(function() {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
-    Route::post("/logout", [ AuthController::class, "logout" ]);
+    Route::post('/logout', [ AuthController::class, 'logout' ]);
+    Route::apiResource('/books', BookController::class);
 });
 
-Route::post("/signup", [ AuthController::class, "signup" ]);
-Route::post("/login", [ AuthController::class, "login" ]);
+Route::post('/signup', [ AuthController::class, 'signup' ]);
+Route::post('/login', [ AuthController::class, 'login' ]);
