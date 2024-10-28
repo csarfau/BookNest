923885d07f233f61cf8695fdde7353f6e9472d5a
César <csarfau@gmail.com>
2024-10-28 03:35:33 -0300

    enhancement: add filter on book querys and add comments in the code

--- backend/app/Http/Controllers/Api/BookController.php
+++ backend/app/Http/Controllers/Api/BookController.php
@@ -7,28 +7,61 @@ use App\Models\Book;
 use App\Http\Requests\StoreBookRequest;
 use App\Http\Requests\UpdateBookRequest;
 use App\Http\Resources\BookResource;
+use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * - Fazendo a busca de todos os livros, e realizando a filtragem pelo título
+     * e também pela descrição, com o mesmo parâmetro de título, para facilitar
+     * as buscas do usuário
+     * - Ordenando por ID e enviando com paginação para o frontend.
+     * - Utilizando Resource personalizada para serializar os dados enviados
      */
-    public function index()
+    public function index(Request $request)
     {
+        if ($request->has('title')) {
+            return BookResource::collection(
+                Book::where(function ($q) use ($request) {
+                    $q->where('title', 'like', '%' . $request->input('title') . '%')
+                        ->orWhere('description', 'like', '%' . $request->input('title') . '%');
+                    })
+                    ->orderBy('id', 'desc')
+                    ->paginate(10)
+            );
+        }
         return BookResource::collection(
             Book::query()->orderBy('id', 'desc')->paginate(10)
         );
     }
 
-    public function userBooks()
+    /**
+     * - Fazendo uma busca apenas dos livros pertencentes ao usuário logado
+     * - Também com filtro de título e descrição facilitando a busca do usuário
+     * - Enviando com paginação e ordenado por ID para o frontend
+     * - Utilizando Resource personalizada para serializar os dados enviados
+     */
+    public function userBooks(Request $request)
     {
+        if ($request->has('title')) {
+            return BookResource::collection(
+                Book::where('user_id', auth()->id())
+                    ->where(function ($q) use ($request) {
+                        $q->where('title', 'like', '%' . $request->input('title') . '%')
+                            ->orWhere('description', 'like', '%' . $request->input('title') . '%');
+                    })
+                    ->orderBy('id', 'desc')
+                    ->paginate(10)
+            );
+        }
         return BookResource::collection(
             Book::where('user_id', (auth()->id()))->orderBy('id', 'desc')->paginate(10)
         );
     }
 
     /**
-     * Store a newly created resource in storage.
+     * - Validando os dados enviados pelo frontend com base na request personalizada
+     * - Cria um novo registro atrelando o livro ao usuário que o criou
      */
     public function store(StoreBookRequest $request)
     {
@@ -40,7 +73,8 @@ class BookController extends Controller
     }
 
     /**
-     * Display the specified resource.
+     * Recebe o livro pelo id, e retorna os dados desse livro, através
+     * do implicit model bind do Laravel
      */
     public function show(Book $book)
     {
@@ -48,7 +82,8 @@ class BookController extends Controller
     }
 
     /**
-     * Update the specified resource in storage.
+     * - Validando os dados enviados pelo frontend com base na request personalizada
+     * - Atualiza o registro do livro com os novos dados
      */
     public function update(UpdateBookRequest $request, Book $book)
     {
